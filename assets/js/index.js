8dff434fc9186b7342eade8a0d01bc981bec31d3
cleverpuss <antakai909@gmail.com>
2022-09-14 08:51:25 +0900

    Replace when clicking person button.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,8 @@ let modal = document.getElementById("myModal");
 // Get the button that opens the modal
 let btn = document.getElementById("myBtn");
 let isOpen = false;
+
+let person = document.getElementsByClassName("person");
 // When the user clicks the button, open the modal
 btn.onclick = function () {
   if (!isOpen) {
@@ -12,3 +14,8 @@ btn.onclick = function () {
   }
   isOpen = !isOpen;
 };
+
+function clickPerson() {
+  console.error("over here!");
+  location.href = "https://api.whatsapp.com/send?phone=51955081075";
+}
